f6f7c4f15b60dccc4b21c865a9fc909cc3bde0c0
jaehyeon <fanta4715@naver.com>
2024-03-16 20:23:16 +0900

    [Chore] init

---
+++ src/main/java/haedal/tdd/instagram/InstagramApplication.java
@@ -0,0 +1,13 @@
+package haedal.tdd.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haedal/tdd/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package haedal.tdd.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
