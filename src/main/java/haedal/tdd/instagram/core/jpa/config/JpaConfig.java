c7384e3488796fb4ea40aacf581e95ee08ec24eb
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-17 23:06:19 +0900

    [Feat]: BaseTimeEntity 추가

---
+++ src/main/java/haedal/tdd/instagram/core/jpa/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package haedal.tdd.instagram.core.jpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
