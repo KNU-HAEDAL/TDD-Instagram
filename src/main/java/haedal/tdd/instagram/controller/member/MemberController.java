d76f866a0bdf832fd89574e9a080c360328730e8
jaehyeon <fanta4715@naver.com>
2024-03-23 17:27:51 +0900

    [Chore] Member Controller 스켈레톤 구현

--- src/main/java/haedal/tdd/instagram/controller/member/MemberController.java
+++ src/main/java/haedal/tdd/instagram/controller/member/MemberController.java
@@ -7,6 +7,8 @@ import haedal.tdd.instagram.controller.member.request.MemberEmailSignupRequest;
 import haedal.tdd.instagram.controller.member.request.MemberUpdateRequest;
 import haedal.tdd.instagram.controller.member.response.MemberDto;
 import haedal.tdd.instagram.controller.member.response.MemberInfoResponse;
+import haedal.tdd.instagram.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class MemberController {
-
+    private final MemberService memberService;
     //1. 회원가입
     @JwtFilterExclusion
     @PostMapping("/member/email-signup")
@@ -27,37 +30,42 @@ public class MemberController {
     }
     //2. 회원탈퇴
     @DeleteMapping("/member")
-    public void deleteMember(@Login Long userId){
+    public void deleteMember(@Login Long memberId){
         throw new RuntimeException();
     }
 
     //3. 정보수정
     @PutMapping("/member")
-    public void updateMember(@Login Long userId, @RequestBody MemberUpdateRequest memberUpdateRequest){
+    public void updateMember(@Login Long memberId, @RequestBody MemberUpdateRequest memberUpdateRequest){
+        memberService.updateMember(memberId, memberUpdateRequest);
         throw new RuntimeException();
     }
 
     //4. 내 정보 확인 (팔로워 숫자 + 개인정보)
     @GetMapping("/member")
-    public MemberInfoResponse getMyInfo(@Login Long userId){
+    public MemberInfoResponse getMyInfo(@Login Long memberId){
+        MemberInfoResponse memberInfo = memberService.getMemberInfo(memberId);
         throw new RuntimeException();
     }
 
     //5. 팔로워 상세조회 페이징 : MemberDto 사용
     @GetMapping("/member/follower")
-    public PagingResponse<MemberDto> getFollowerPaging(@Login Long userId, int page, int size){
+    public PagingResponse<MemberDto> getFollowerPaging(@Login Long memberId, int page, int size){
+        PagingResponse<MemberDto> followerPaging = memberService.getFollowerPaging(memberId, page, size);
         throw new RuntimeException();
     }
 
     //6. 팔로잉 상세조회 페이징 : MemberDto 사용
     @GetMapping("/member/following")
-    public PagingResponse<MemberDto> getFollowingPaging(@Login Long userId, int page, int size){
+    public PagingResponse<MemberDto> getFollowingPaging(@Login Long memberId, int page, int size){
+        PagingResponse<MemberDto> followingPaging = memberService.getFollowingPaging(memberId, page, size);
         throw new RuntimeException();
     }
 
     //7. 내 게시글 확인 (사진만 페이징) : List<String>으로 imageUrl
     @GetMapping("/member/article")
-    public PagingResponse<String> getMyArticlePaging(@Login Long userId, int page, int size){
+    public PagingResponse<String> getMyArticlePaging(@Login Long memberId, int page, int size){
+        PagingResponse<String> myArticlePaging = memberService.getMyArticlePaging(memberId, page, size);
         throw new RuntimeException();
     }
 }
